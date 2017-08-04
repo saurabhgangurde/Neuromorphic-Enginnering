1bef8f1d5a4b53a61b52b3f94eab5e079838bd0b
saurabhgangurde <saurabhgangurde5596@gmail.com>
2017-08-04 21:18:12 +0530

    modified vpeak correctly

--- Assignment1/rk4.m
+++ Assignment1/rk4.m
@@ -1,37 +1,40 @@
-function [y,z] = rk4(delta_t,T,input,neuron_type)
+function [V,U] = rk4(delta_t,T,input,neuron_type)
 
     [C,kz,Er,Et,a,b,c,d,v_peak]=neuron_data(neuron_type);
     polynom = [kz/c,-1-((Er+Et)*kz/b),Er+Er*Et*kz/b];
-    root_temp=roots(polynom)
+    root_temp=roots(polynom);
     
     x = 0:delta_t:T;                                         % Calculates upto y(3)
-    y = zeros(size(input,1),size(x,2)); 
-    z = zeros(size(input,1),size(x,2)); 
+    V = zeros(size(input,1),size(x,2)); 
+    U = zeros(size(input,1),size(x,2)); 
     
-    y(:,1) = root_temp(2);                                          % initial condition
-    z(:,1) = (root_temp(2)-Er)*b;                                          % initial condition
+    V(:,1) = root_temp(2);                                          % initial condition
+    U(:,1) = (root_temp(2)-Er)*b;                                          % initial condition
     
-    F_xy = @(t,r,p) (1/C)*(kz*(r^2-(Et+Er)*r+Et*Er)-p+t);% change the function as you desire
-    G_xy = @(t,r,p) a*(b*(r-Er)-p);
+    F_xy = @(I,v,u) (1/C)*(kz*(v.^2-(Et+Er)*v+Et*Er)-u+I);% change the function as you desire
+    G_xy = @(I,v,u) a*(b*(v-Er)-u);
     
     %% loop for V calc
-    for j=1:(size(input,1))
-        for i=1:size(x,2)-1 
-            k0=F_xy(input(1,i),y(1,i),z(1,i)); 
-            l0=G_xy(input(1,i),y(1,i),z(1,i));
-            k1=F_xy(input(1,i),y(1,i)+0.5*k0*delta_t,z(1,i)+0.5*l0*delta_t);
-            l1=G_xy(input(1,i),y(1,i)+0.5*k0*delta_t,z(1,i)+0.5*l0*delta_t);
-            k2=F_xy(input(1,i),y(1,i)+0.5*k1*delta_t,z(1,i)+0.5*l1*delta_t);
-            l2=G_xy(input(1,i),y(1,i)+0.5*k1*delta_t,z(1,i)+0.5*l1*delta_t);
-            k3=F_xy(input(1,i),y(1,i)+k2*delta_t,z(1,i)+l2*delta_t);
-            l3=G_xy(input(1,i),y(1,i)+k2*delta_t,z(1,i)+l2*delta_t);
+for i=1:size(x,2)-1 
+            k0=F_xy(input(:,i),V(:,i),U(:,i)); 
+            l0=G_xy(input(:,i),V(:,i),U(:,i));
+            k1=F_xy(input(:,i),V(:,i)+0.5*k0*delta_t,U(:,i)+0.5*l0*delta_t);
+            l1=G_xy(input(:,i),V(:,i)+0.5*k0*delta_t,U(:,i)+0.5*l0*delta_t);
+            k2=F_xy(input(:,i),V(:,i)+0.5*k1*delta_t,U(:,i)+0.5*l1*delta_t);
+            l2=G_xy(input(:,i),V(:,i)+0.5*k1*delta_t,U(:,i)+0.5*l1*delta_t);
+            k3=F_xy(input(:,i),V(:,i)+k2*delta_t,U(:,i)+l2*delta_t);
+            l3=G_xy(input(:,i),V(:,i)+k2*delta_t,U(:,i)+l2*delta_t);
             
-            y(1,i+1)=y(1,i)+(1/6)*(k0+2*k1+2*k2+k3)*delta_t;
-            z(1,i+1)=z(1,i)+(1/6)*(l0+2*l1+2*l2+l3)*delta_t;
+            V(:,i+1)=V(:,i)+(1/6)*(k0+2*k1+2*k2+k3)*delta_t;
+            U(:,i+1)=U(:,i)+(1/6)*(l0+2*l1+2*l2+l3)*delta_t;
 
-            if y(1,i+1)>v_peak
-                        y(1,i+1)=root_temp(2);
-            end
-        end
-    end
+            % spiking 
+            temp=V(:,i+1);
+            index=find(temp>20*10^-3);
+            temp(temp>20*10^-3)=c;
+            V(:,i+1)=temp;
+            V(index,i)=v_peak;
+            
+            U(index,i+1)=U(index,i+1)+d;
+end
 end
