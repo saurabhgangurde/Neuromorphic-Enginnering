3187177ec9a0b9678aa02f8cb6e87a41ef03d41d
saurabhgangurde <saurabhgangurde5596@gmail.com>
2017-08-28 20:16:26 +0530

    Q1 and Q2 mostly completed

--- Assignment2/Q2.m
+++ Assignment2/Q2.m
@@ -15,6 +15,62 @@ myPoissonSpikeTrain = rand(Ns, steps) < lambda*delta_t;
 
 
 Io=1E-12;
+Wo=50;
+sigma_w=5;
+tau=25*ms;
+taus=tau/4;
+t=0:delta_t:T;
+Iapp_global=zeros(size(t));
+
+for k=1:1:Ns
+
+    tm=find(myPoissonSpikeTrain(k,:)==1)*0.1*ms;
+    Iapp_synopse=zeros(size(t));
+
+        for j=1:size(t,2)
+            temp=0;
+           for i=1:size(tm,2)
+
+               if t(j)>tm(i)
+                   temp=temp+exp((tm(i)-t(j))/tau)-exp((tm(i)-t(j))/taus);
+               end
+               
+
+           end
+           Iapp_synopse(j)=temp;
+
+        end
+     Iapp_global=Iapp_global+(Wo+sigma_w*randn())*Iapp_synopse;
+end
+
+Iapp_global=Io*Iapp_global;
+
+[V,U] = AEF(delta_t,T,Iapp_global,1);
+
+
+figure();
+plot(t*1E3,V);
+xlabel('Time in mS');ylabel('Voltage in Volts');
+title('Voltage Vs Time');
+
+figure();
+plot(t*1E3,Iapp_global*1E12);
+xlabel('Time in mS');ylabel('Current in pA');
+title('Current Vs Time');
+%% Q1 Part-B
+
+ms=1E-3;
+T=500*ms;
+delta_t=0.1*ms;
+steps=T/delta_t;
+Ns=100;
+lambda=1;
+
+
+myPoissonSpikeTrain = rand(Ns, steps) < lambda*delta_t;
+
+
+Io=1E-12;
 Wo=250;
 sigma_w=15;
 tau=25*ms;
@@ -46,7 +102,14 @@ end
 Iapp_global=Io*Iapp_global;
 
 [V,U] = AEF(delta_t,T,Iapp_global,1);
-plot(t,V);
 
-%% Q1 Part-B
 
+figure();
+plot(t*1E3,V);
+xlabel('Time in mS');ylabel('Voltage in Volts');
+title('Voltage Vs Time');
+
+figure();
+plot(t*1E3,Iapp_global*1E12);
+xlabel('Time in mS');ylabel('Current in pA');
+title('Current Vs Time');
