c5a78a6e4b2e6bf8525512e3c61f907c58dccefd
saurabhgangurde <saurabhgangurde5596@gmail.com>
2017-08-28 20:14:02 +0530

    Q1 and Q2 corrected

--- Assignment2/Q1.m
+++ Assignment2/Q1.m
@@ -2,29 +2,24 @@
 
 %% Q1 Part-A
 
-seed=10;
+seed=100;
 rng(seed)
-T=500;
-delta_t=0.1;
+ms=1E-3;
+
+T=500*ms;
+delta_t=0.1*ms;
 steps=T/delta_t;
 lambda=10;
 
-random_sample = poissrnd(lambda,1,steps);
-
-random_sample_cumsum=cumsum(random_sample);
-
-indices=find(random_sample_cumsum<T);
-
-poisson_random_spikes=zeros(1,500);
-poisson_random_spikes(random_sample_cumsum(indices))=1;
+myPoissonSpikeTrain = rand(1, steps) < lambda*delta_t;
 
 %% Q1 Part-B
 Io=1E-12;
 We=500;
-tau=15;
+tau=15*ms;
 taus=tau/4;
 t=0:delta_t:T;
-tm=random_sample_cumsum(indices);
+tm=find(myPoissonSpikeTrain==1)*0.1*ms;
 i_matrix=zeros(size(tm,2),size(t,2));
 
 Iapp=zeros(size(t));
@@ -32,18 +27,32 @@ Iapp=zeros(size(t));
 for j=1:size(t,2)
     temp=0;
    for i=1:size(tm,2)
-       
-       if t(j)>tm(i)
+      
+       if (t(j)>tm(i))
+           
            temp=temp+exp((tm(i)-t(j))/tau)-exp((tm(i)-t(j))/taus);
        end
-       Iapp(j)=temp;
-   
+          
    end
+
+   Iapp(j)=temp;
     
 end
+
 Iapp=Io*We*Iapp;
 
 
-[V,U] = AEF(delta_t*1E-3,T*1E-3,Iapp,1);
+[V,U] = AEF(delta_t,T,Iapp,1);
 
-plot(t,V);
+figure();
+plot(t*1E3,V);
+xlabel('Time in mS');ylabel('Voltage in Volts');
+title('Voltage Vs Time');
+figure();
+plot(t(2:end)*1E3,myPoissonSpikeTrain);
+xlabel('Time in mS');ylabel('Spike');
+title('spike Vs Time');
+figure();
+plot(t*1E3,Iapp*1E12);
+xlabel('Time in mS');ylabel('Current in pA');
+title('Current Vs Time');
--- Assignment2/Q2.m
+++ Assignment2/Q2.m
@@ -2,28 +2,29 @@
 
 %% Q1 Part-A
 
-seed=10;
-rng(seed)
-T=500;
-delta_t=0.1;
+
+ms=1E-3;
+T=500*ms;
+delta_t=0.1*ms;
 steps=T/delta_t;
 Ns=100;
 lambda=1;
 
-random_sample = poissrnd(lambda,Ns,steps);
 
-random_sample_cumsum=cumsum(random_sample,2);
+myPoissonSpikeTrain = rand(Ns, steps) < lambda*delta_t;
+
 
 Io=1E-12;
 Wo=250;
-sigma_w=25;
-tau=15;
+sigma_w=15;
+tau=25*ms;
 taus=tau/4;
 t=0:delta_t:T;
 Iapp_global=zeros(size(t));
+
 for k=1:1:Ns
-    indices=find(random_sample_cumsum(k,:)<T);
-    tm=random_sample_cumsum(k,indices);
+
+    tm=find(myPoissonSpikeTrain(k,:)==1)*0.1*ms;
     Iapp_synopse=zeros(size(t));
 
         for j=1:size(t,2)
@@ -33,9 +34,10 @@ for k=1:1:Ns
                if t(j)>tm(i)
                    temp=temp+exp((tm(i)-t(j))/tau)-exp((tm(i)-t(j))/taus);
                end
-               Iapp_synopse(j)=temp;
+               
 
            end
+           Iapp_synopse(j)=temp;
 
         end
      Iapp_global=Iapp_global+(Wo+sigma_w*randn())*Iapp_synopse;
@@ -43,7 +45,7 @@ end
 
 Iapp_global=Io*Iapp_global;
 
-[V,U] = AEF(delta_t*1E-3,T*1E-3,Iapp_global,1);
+[V,U] = AEF(delta_t,T,Iapp_global,1);
 plot(t,V);
 
 %% Q1 Part-B
