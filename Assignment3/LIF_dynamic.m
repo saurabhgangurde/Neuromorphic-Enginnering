4d51c3e864a6d761174a1f7735ae8aff846c87a8
saurabhgangurde <saurabhgangurde5596@gmail.com>
2017-09-19 23:09:38 +0530

    Q3 in debug

--- Assignment3/LIF_dynamic.m
+++ Assignment3/LIF_dynamic.m
@@ -1,58 +1,45 @@
-function [ y,x,spikes] = LIF_dynamic( delta_t,T,N,Fanout,Weight,Delay,EL,gL,C,Vt,Iext)
+function [ V,t,spikes] = LIF_dynamic( delta_t,T,N,fanout_matrix,Weights_matrix,delay_matrix,EL,gL,C,Vt,Iext)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    x = linspace(0,T,T/delta_t);                                         % Calculates upto y(3)
-    y = zeros(N,size(x,2)); 
-    y(:,1) = EL;                                          % initial condition
-    F_xy = @(t,r) (1/C)*(-gL*(r-EL)+t);                    % change the function as you desire
-    
-    spikes_time=-100*ones(size(y,1),1);
-    spikes=zeros(N,T/delta_t);
-    Iapp=zeros(N,T/delta_t);
-    Iapp(1:size(Iext,1),:)=Iext;
-    
     Io=1E-12;
     tau=15E-3;
     tau_s=tau/4;
-    
+
+    t=linspace(0,T,T/delta_t);
+    V = zeros(N,size(t,2)); 
+    V(:,1) = EL;                                          % initial condition
+    F = @(I,r) (1/C)*(-gL*(r-EL)+I);                    % change the function as you desire
+    spikes=zeros(N,T/delta_t);
+    last_spike_time=-100*ones(size(V,1),1);
+    Iapp=zeros(size(V,1),1);
     Isyn_t= @(we,tk,td,t) Io*we*(exp(-(t-tk-td)/tau)-exp(-(t-tk-td)/tau_s)).*(t>tk+td);
+    Iapp(1:size(Iext,1))=Iext(:,1);
+    for time_t=1:T/delta_t-1
+        time_t
+        k_1 = F(Iapp,V(:,time_t));
+        V(:,time_t+1) = V(:,time_t) + k_1*delta_t;  % main equation
+        time_t_V=V(:,time_t+1);
+        spiking_neurons=find(time_t_V>Vt);
 
-    for iter=1:size(x,2)-1                            % calculation loop for different time intervals
-              
-            k_1 = F_xy(Iapp(:,iter),y(:,iter));
-            k_2 = F_xy(Iapp(:,iter),y(:,iter)+delta_t*k_1);
+        time_t_V(time_t_V>Vt |last_spike_time+2E-3/delta_t>time_t )=EL;    %% refractory period involved
 
-            y(:,iter+1) = y(:,iter) + (1/2)*(k_1+2*k_2)*delta_t;  % main equation
-            %y(:,iter+1) = y(:,iter) + k_1*delta_t;  % main equation
-            
-            
-            % determining which neuron will spike at ith interval
-            temp=y(:,iter+1);
-            index=find(temp>Vt);
-            
-            
-            temp(temp>Vt |spikes_time+2E-3/delta_t>iter )=EL;    %% refractory period involved
-            spikes_time(index)=iter;
-            
-            y(:,iter+1)=temp;
-            y(index,iter)=70E-3;
-            spikes(index,iter)=1;
-            % modified Iapp calculation
-            
-            Isyn=zeros(N,T/delta_t);
-            for i=1:N
-                fanout_nodes=Fanout{i};
-                temp_spike_time=find(spikes(i,1:iter)==1)*delta_t;
-                for j=1:size(fanout_nodes,2)                    
-                    for k=1:size(temp_spike_time,2)
-                        Isyn(fanout_nodes(j),iter+1)=Isyn(fanout_nodes(j),iter+1)+...
-                                                     Isyn_t(Weight{i}(j),temp_spike_time(k),Delay{i}(j),x(iter+1));
-                    end
+        V(:,time_t+1)=time_t_V;
+        V(spiking_neurons,time_t)=70E-3;
+        last_spike_time(spiking_neurons)=time_t;
+        spikes(spiking_neurons,time_t)=1;
+
+        Isyn=zeros(size(V,1),1);
+        for i=1:N
+            current_neuron_spikes=find(spikes(i,1:time_t)==1)*delta_t;
+            for j=1:size(fanout_matrix(i,:),2)
+                for k=1:size(current_neuron_spikes,2)
+                    Isyn(fanout_matrix(i,j))=Isyn(fanout_matrix(i,j))+Isyn_t(Weights_matrix(i,j),current_neuron_spikes(k),...
+                                                                delay_matrix(i,j),t(time_t));
                 end
             end
-            Iapp(:,iter+1)=Iapp(:,iter+1)+Isyn(:,iter+1);    
-                
-
+        end
+        Iapp(1:size(Iext,1))=Iext(:,time_t+1)+Isyn(1:size(Iext,1));
+        Iapp(size(Iext,1)+1:end)=Isyn(size(Iext,1)+1:end);
     end
 
 end
--- Assignment3/Q2.m
+++ Assignment3/Q2.m
@@ -2,32 +2,31 @@
 
 %% Part A
 seed=200;
-rng(seed);
+rng(seed,'twister');
 
 ms=1E-3;
 N=500;
-
+fanout_ratio=N/10;
+Ne=N*0.80;
+Ni=N-Ne;
 %  creating network
-fanout_matrix=zeros(50,N);
-for i=1:round(N*0.8)
-    fanout_matrix(:,i)=randperm(N,50);
+fanout_matrix=zeros(N,fanout_ratio);
+for i=1:Ne
+    fanout_matrix(i,:)=randperm(N,fanout_ratio);
 end
-for i=round(N*0.8)+1:N
-    fanout_matrix(:,i)=randperm(round(N*0.8),50);
+for i=Ne+1:N
+    fanout_matrix(i,:)=randperm(Ne,fanout_ratio);
 end
-Fanout=num2cell(fanout_matrix,1);
 
-Weights_matrix=3000*ones(50,N);
-Weights_matrix(:,round(N*0.8)+1:end)=-3000;
-Weight=num2cell(Weights_matrix,1);
+Weights_matrix=3000*ones(N,fanout_ratio);
+Weights_matrix(Ne+1:end,:)=-3000;
 
-delay_matrix=randi([1,20],[50,N])*ms;
-delay_matrix(:,round(N*0.8)+1:end)=1*ms;
-Delay=num2cell(delay_matrix,1);
+delay_matrix=randi([1,20],[N,fanout_ratio])*ms;
+delay_matrix(round(N*0.8)+1:end,:)=1*ms;
 
 % constants
-delta_t=0.5*ms;
-T=1000*ms;
+delta_t=1*ms;
+T=500*ms;
 t=linspace(0,T,T/delta_t);
 Io=1E-12;
 tau=15*ms;
@@ -40,6 +39,7 @@ Rp=2*ms;
 ws=3000;
 
 % % forming Iext matrix
+tic
 lambda=100;
 myPoissonSpikeTrain = rand(25, T/delta_t) < lambda*delta_t;
 Iext_t= @(ts,t) Io*ws*(exp(-(t-ts)/tau)-exp(-(t-ts)/tau_s)).*(t>ts);
@@ -52,9 +52,11 @@ for i=1:25
 
 end
 
-[V,t,spikes]=LIF_dynamic( delta_t,T,N,Fanout,Weight,Delay,EL,gL,C,Vt,Iext);
-plotRaster(spikes,t);
 
+[V,t,spikes]=LIF_dynamic( delta_t,T,N,fanout_matrix,Weights_matrix,delay_matrix,EL,gL,C,Vt,Iext);
+
+imshow(spikes*255);
+plotRasster(spikes,t);
 Re_temp=sum(spikes(1:round(N*0.8),:),1);
 Ri_temp=sum(spikes(round(N*0.8)+1:end,:),1);
 Re=zeros(1,T/delta_t-10*ms/delta_t);
