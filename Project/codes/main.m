160bbe2b4a0fca757052d401f55bd45e2a6aa14f
saurabhgangurde <saurabhgangurde5596@gmail.com>
2017-10-05 15:14:46 +0530

    paper almost implemented

--- Project/codes/main.m
+++ Project/codes/main.m
@@ -11,16 +11,16 @@ T=0.6*us;
 delta_t=1*ns;
 t=0:delta_t:T;
 Vth=1;
-Cm=1E-9;
-R=1E3;
+Cm=1E-10;
+R=1E6;
 
-spikes=zeros(Ntotal,size(t,2));
+
 
 Weights=500E3*rand(Ntotal,4);    % 500K = max Resistance state of synapse
 Weights(Nin+1:end,:)=0.001;
 Weights_init=Weights;
 %SET_threshold=normrnd(1.95,0.15,[Ntotal,4]);
-SET_threshold=1.65+rand([Ntotal,4]);
+SET_threshold=1.5+rand([Ntotal,4]);
 RESET_threshold=-1.7;
 
 load('./database/database.mat')
@@ -34,7 +34,8 @@ for epoch=1:1
 
         image=database{1,iter};
         input=reshape(image,[32*32,1]);
-        spiking_neurons=find(input>150);
+        spikes=zeros(Ntotal,size(t,2));
+        spiking_neurons=find(input>100);
         spikes(spiking_neurons,1:500)=ones(size(spiking_neurons))*weak_spike;
         Voltages=zeros(Nout,size(t,2));
         Isyn=zeros(4,size(t,2));
@@ -47,8 +48,8 @@ for epoch=1:1
         for time_t=1:size(t,2)-1
             %time_t
             for output=1:4
-                Isyn(output,time_t)=sum((spikes(1:Nin,time_t)-spikes(Nin+output,time_t))./Weights(1:Nin,output))...
-                                    -sum((spikes(Nin+1:end,time_t)-spikes(Nin+output,time_t))./500);
+                Isyn(output,time_t)=sum((spikes(1:Nin,time_t)-spikes(Nin+output,time_t))./Weights(1:Nin,output));...
+                                    %-sum((spikes(Nin+1:end,time_t)-spikes(Nin+output,time_t))./500);
 
 
                 if winning_spike+20<time_t 
@@ -74,7 +75,8 @@ for epoch=1:1
                 for input=1:Nin
 
                     if spikes(input,time_t)-spikes(Nin+output,time_t)>SET_threshold(input,output)  % SET condition
-                        Weights(input,output)=500;
+                        %fprintf('here SET\n');
+                        Weights(input,output)=5000;
                     else if spikes(input,time_t)-spikes(Nin+output,time_t)<RESET_threshold         % RESET condition
                          %fprintf('here RESET\n');
                          %SET_threshold(input,output)=normrnd(1.95,0.3);
@@ -89,15 +91,16 @@ for epoch=1:1
             end
 
         end
+
     end
 end
         
 max_resistance=500E3;          
-figure()
+figure(1)
 imshow([uint8(reshape(Weights_init(1:32*32,1),[32,32])/max_resistance*255) uint8(reshape(Weights(1:32*32,1),[32,32])/max_resistance*255)])
-figure()
+figure(2)
 imshow([uint8(reshape(Weights_init(1:32*32,2),[32,32])/max_resistance*255) uint8(reshape(Weights(1:32*32,2),[32,32])/max_resistance*255)])
-figure()
+figure(3)
 imshow([uint8(reshape(Weights_init(1:32*32,3),[32,32])/max_resistance*255) uint8(reshape(Weights(1:32*32,3),[32,32])/max_resistance*255)])
-figure()
+figure(4)
 imshow([uint8(reshape(Weights_init(1:32*32,4),[32,32])/max_resistance*255) uint8(reshape(Weights(1:32*32,4),[32,32])/max_resistance*255)])
